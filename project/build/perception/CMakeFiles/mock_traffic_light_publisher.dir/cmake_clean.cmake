955d4dc7395aab968887e92d17f128b737fbb6cd
Zhiyu Zhao <zhiyu.zhao@tum.de>
2025-06-26 00:59:27 +0200

    complete initialization

---
+++ project/build/perception/CMakeFiles/mock_traffic_light_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/zzy/introtoros_2025-main-project/project/devel/.private/perception/lib/perception/mock_traffic_light_publisher"
+  "/home/zzy/introtoros_2025-main-project/project/devel/.private/perception/lib/perception/mock_traffic_light_publisher.pdb"
+  "CMakeFiles/mock_traffic_light_publisher.dir/src/mock_traffic_light_publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mock_traffic_light_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- project/build/perception/catkin_generated/setup_cached.sh
+++ project/build/perception/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/zzy/introtoros_2025-main-project/project/devel/.private/perception:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/zzy/introtoros_2025-main-project/project/devel/.private/perception/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/zzy/introtoros_2025-main-project/project/devel/.private/perception/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/zzy/introtoros_2025-main-project/project/build/perception'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/zzy/introtoros_2025-main-project/project/devel/.private/perception/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/zzy/introtoros_2025-main-project/project/src/perception:$ROS_PACKAGE_PATH"
